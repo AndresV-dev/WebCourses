d32d7163aa7e7dd4b3168153d78e9adf911c9e9f
Andres Vargas <avargas@applab.mx>
2023-06-02 17:27:48 -0600

    Fetch module

--- NodeJs/math/index.js
+++ NodeJs/math/index.js
@@ -1,11 +1,19 @@
-const add = (x, y) => x + y;
-const substract = (x, y) => x - y;
-const divide = (x, y) => x / y;
-const multiply = (x, y) => x * y;
+const add = (x, y) => x + y;      // funcion flecha devuelve la Suma de 2 numeros
+const substract = (x, y) => x - y;// funcion flecha devuelve la Resta de 2 numeros
+const divide = (x, y) => x / y;   // funcion flecha devuelve la Divicion de 2 numeros
+const multiply = (x, y) => x * y; // funcion flecha devuelve la Multiplicacion de 2 numeros
 
-module.exports = {
+// con la implementacio del "type":"module" al package.json se agrega la posibilidad de exportar de una manera diferente lo cual es equivalente a la forma nativa
+export default {
     add,
     substract,
     divide,
-    multiply
+    multiply    
 }
+
+// module.exports = {
+//     add,
+//     substract,
+//     divide,
+//     multiply
+// }
