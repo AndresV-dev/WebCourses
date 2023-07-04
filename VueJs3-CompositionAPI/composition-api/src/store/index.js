42b8db6c2785598084b67d9b9ddc1aa2f384859e
Andres Vargas <wanesvargas@gmail.com>
2023-07-03 22:12:03 -0600

    Proyect Creaton, to learn store manipulation

---
+++ VueJs3-CompositionAPI/composition-api/src/store/index.js
@@ -0,0 +1,10 @@
+
+import { reactive } from "vue"
+
+const state = reactive({
+        counter: 0
+    })
+
+export default{
+    state
+}
---
+++ VueJs3-CompositionAPI/composition-api/src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <p>Hola</p>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="counter"> {{ counter }} </div>
+    <div class="buttons">
+      <button @click="decreaseCounter()">-</button>
+      <button @click="increaseCounter()">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HomeView',
+  setup() {
+    let counter = ref(0);
+
+    const increaseCounter = () => {
+      counter.value++
+    }
+    const decreaseCounter = () => {
+      counter.value--
+    }
+
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter
+    }
+  }
+}
+</script>
+
+<style>
+div {
+  margin-bottom: 10px;
+}
+
+.counter {
+  font-size: 80px;
+}
+
+.buttons {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
