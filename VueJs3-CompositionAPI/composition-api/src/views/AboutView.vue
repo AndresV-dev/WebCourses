b80c1f15a0886e054ffd9415521d03e9d2e45395
Andres Vargas <wanesvargas@gmail.com>
2023-07-07 18:31:51 -0600

    Counter Functions declared for all aplication

--- VueJs3-CompositionAPI/composition-api/src/views/AboutView.vue
+++ VueJs3-CompositionAPI/composition-api/src/views/AboutView.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <p>Hola</p>
+    <h1>Counter can Be Usefull everyware</h1>
+    <p>{{ store.state.counter }}</p>
   </div>
 </template>
 
+<script>
+import { inject } from 'vue';
+
+export default {
+  setup() {
+    //Se crea una constante llamada store a la cual se le asigna la injeccion de el store
+    const store = inject('store')
+
+    return {
+      store
+    }
+  }
+}
+</script>
--- VueJs3-CompositionAPI/composition-api/src/views/HomeView.vue
+++ VueJs3-CompositionAPI/composition-api/src/views/HomeView.vue
@@ -2,33 +2,45 @@
   <div class="home">
     <p>Hola</p>
     <img alt="Vue logo" src="../assets/logo.png">
-    <div class="counter"> {{ counter }} </div>
+    <!-- Al llamar tabien en la pagina de about el contador, este podra almacenar si incrementamos o no en la home page, y
+    tambien lo mostrara en esta pagina -->
+    <div :style="{ color: store.state.colorCode }" class="counter"> {{ store.state.counter }} </div>
+    <div class="counter-scuared">
+      {{ store.state.counter }}
+      <sup>2</sup> =
+      {{ store.getters.counterScuared() }}
+    </div>
     <div class="buttons">
-      <button @click="decreaseCounter()">-</button>
-      <button @click="increaseCounter()">+</button>
+      <button @click="store.methods.decreaseCounter()">-</button>
+      <button @click="store.methods.increaseCounter()">+</button>
+    </div>
+    <div>
+      <!--Con el v-model podemos asignar una variable guardada en nuestro state para asi dejar al usuario poder asignar un valor modificable, en este caso el color del numero, esto solo si el store esta desprotegido de lo contrario el store debera de estar en readOnly por lo cual no se podra modificar, sin una funcion-->
+      <input v-model="colorCode" type="text" name="ChangeColor" id="ChangeColor" placeholder="Enter Color Code">
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { inject, computed } from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
-    let counter = ref(0);
+    const store = inject('store')
+    const colorCode = computed({
+      get() {
+        return store.state.colorCode
+      },
+      set(color) {
+        store.methods.setColorCode(color)
+      }
+    })
 
-    const increaseCounter = () => {
-      counter.value++
-    }
-    const decreaseCounter = () => {
-      counter.value--
-    }
-
+    console.log(store.methods)
     return {
-      counter,
-      increaseCounter,
-      decreaseCounter
+      store,
+      colorCode
     }
   }
 }
