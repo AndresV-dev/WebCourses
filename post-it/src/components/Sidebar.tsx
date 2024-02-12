d405ff58c9d466f5e83987553a63d9a756c2ab3a
Andres Vargas <wanesvargas@gmail.com>
2024-02-11 21:20:36 -0600

    Add a class with the types of data to manage, Task, User, etc

--- post-it/src/components/Sidebar.tsx
+++ post-it/src/components/Sidebar.tsx
@@ -1,26 +1,18 @@
 import '../assets/css/sidebar.css';
+import { User } from '../types';
 
-function Sidebar() {
-  //for test now
-  const user = {
-    id: 1,
-    uuid: "UD25F-6DH21-1JNHD-143H",
-    name: "Andres",
-    lastname: "Vargas",
-    edad: 26,
-    email: "avargas@gmail.com",
-    username: "AndresVargas",
-    password: "hjsdgfhjsdj32476jknsadbf",
-    userImage: "https://cdn-icons-png.flaticon.com/512/6326/6326055.png",
-  };
+interface SidebarProps {
+  user: User
+}
 
+function Sidebar(props: SidebarProps) {
   return (
     <nav className="sidebar">
       <ul className="user">
         <li>
-          <img src="https://cdn-icons-png.flaticon.com/512/6326/6326055.png" alt="User Profile Image" />
+          <img src={props.user.userImage} alt="User Profile Image" />
         </li>
-        <li>{user.username}</li>
+        <li>{props.user.username}</li>
       </ul>
       <ul className="">
         <li className="links">Add Task</li>
---
+++ post-it/src/types.ts
@@ -0,0 +1,46 @@
+export interface User {
+    id: number,
+    uuid: string,
+    role: string,
+    name: string,
+    lastname: string,
+    age: number,
+    username: string,
+    password: string,
+    email: string,
+    json?: JSON,
+    created_at: Date,
+    userImage?: string,
+    taskCollections?: UserTaskCollections[]
+}
+
+export interface UserTaskCollections {
+    id: number,
+    description: string,
+    name: string,
+    user_id: number
+}
+
+export interface Task {
+    id: number,
+    uuid: string,
+    tittle: string,
+    description: string,
+    created_at: Date,
+    end_at?: Date,
+    user_id: number,
+    priority_id?: number,
+    collection_id?: number,
+    category_id?: number
+}
+
+export interface TaskPriority {
+    id: number,
+    description: string,
+    name: string
+}
+
+export enum Role {
+    'ADMIN',
+    'USER'
+}
