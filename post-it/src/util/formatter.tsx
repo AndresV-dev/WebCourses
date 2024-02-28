d842b2e059afe1db9741ae4d80de5d594c317e03
Andres Vargas <wanesvargas@gmail.com>
2024-02-27 23:27:01 -0600

    Add a second section with Tomorrow Tasks and a message when you dont have any

---
+++ post-it/src/util/formatter.tsx
@@ -0,0 +1,25 @@
+export default function formatDate(date: Date, withTime: boolean) {
+    let dateFormated = [
+        date.getFullYear(),
+        padTo2Digits(date.getMonth() + 1),
+        padTo2Digits(date.getDate()),
+      ].join('-');      
+
+      withTime? dateFormated + ' ' + // Validate If need to add the time on the variable
+      [
+       padTo2Digits(date.getHours()),
+       padTo2Digits(date.getMinutes()),
+       padTo2Digits(date.getSeconds()),
+     ].join(':')
+     :
+     "";
+
+     return (
+        dateFormated
+    );
+  }
+
+  // Add 0 if the date is only one digit
+function padTo2Digits(num: number) {
+    return num.toString().padStart(2, '0');
+}
