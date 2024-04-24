13555b31b2b6765344a33b418bfcaa458d98c15a
Andres Vargas <wanesvargas@gmail.com>
2024-04-23 22:49:27 -0600

    Fix and error on Dashboard, some api was called every time the page charge, even the variables are filled, now if the variableas are filled the calls will not happen

--- post-it/src/pages/TasksView.tsx
+++ post-it/src/pages/TasksView.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { Task } from "../types";
+import TaskList from "../components/TaskList";
+import Header from "../components/Header";
 
 export default function TasksView() {
   let { state } = useLocation();
+  let filters = {
+    collection: state.collectionId || 0,
+    category: state.categoryId || 0,
+  };
+
+  const [task, setTask] = useState<Array<Task>>([]);
   return (
     <div>
-      This is a New Page for the tasks with category: {state.collectionId} and a test Prop: {state.test}{" "}
+      {task.length == 0 ? (
+        <div className="todayTask">
+          <Header
+            label="general"
+            getTask={(task) => {
+              setTask(task);
+            }}
+          />
+          <TaskList tasks={task} />
+        </div>
+      ) : undefined}
     </div>
   );
 }
