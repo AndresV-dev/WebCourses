af1938b10d638d1b90686031bde38f59218bb121
Andres Vargas <wanesvargas@gmail.com>
2024-11-07 23:41:21 -0600

    Add the function to add to a list the ids to delete

--- post-it/src/components/Task.tsx
+++ post-it/src/components/Task.tsx
@@ -1,19 +1,39 @@
 import { Category, Task as TaskType } from "../types";
+import Button from "./Button";
 
 interface TaskData {
   task: TaskType;
+  delete: Boolean;
+  getTaskdeleteIds: (ids: number[]) => void;
 }
 
 function Task(props: TaskData) {
   const categories: Category[] = JSON.parse(sessionStorage.categories);
+  const taskIds: number[] = [];
+
   return (
     <div className={"task " + props.task.status}>
-      <h3 className="task-title">{props.task.title}</h3>
       <div>
-        <p className="task-description">{props.task.description}</p>
-        <p className="task-date">{props.task.endAt.toString()}</p>
+        {props.delete ? (
+          <Button
+            label={""}
+            type="checkbox"
+            key={props.task.id}
+            onClick={() => {
+              taskIds.push(props.task.id);
+              props.getTaskdeleteIds(taskIds);
+            }}
+          />
+        ) : undefined}
       </div>
-      <p className="task-category">{categories.length != undefined ? categories.find((cat) => cat.id === props.task.categoryId)?.name || "General" : "Data Not Available"}</p>
+      <div>
+        <h3 className="task-title">{props.task.title}</h3>
+        <div>
+          <p className="task-description">{props.task.description}</p>
+          <p className="task-date">{props.task.endAt.toString()}</p>
+        </div>
+        <p className="task-category">{categories.length != undefined ? categories.find((cat) => cat.id === props.task.categoryId)?.name || "General" : "Data Not Available"}</p>
+      </div>
     </div>
   );
 }
